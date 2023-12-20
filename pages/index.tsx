14e420ad5b35b140eae27fd69ee33a0dcd255d65
Filippo Benozzi <filippo.benozzi@dinamiza.it>
2023-12-20 15:18:00 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,5 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import Head from 'next/head';
-
-import avatar from '../public/avatar.jpg';
-
-const ConListItem = ({ children }) => (
-  <li className="flex items-center my-1">
-    <svg
-      className="h-5 text-red-500 mr-2"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-    </svg>
-    <span className="text-lg font-medium">{children}</span>
-  </li>
-);
-
-const ProListItem = ({ children }) => (
-  <li className="flex items-center my-1">
-    <svg
-      className="h-5 text-green-500 mr-2"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-    </svg>
-    <span className="text-lg font-medium">{children}</span>
-  </li>
-);
 
 export default function Index() {
   const meta = {
@@ -54,7 +11,7 @@ export default function Index() {
   return (
     <>
       <Head>
-        <title>{meta.title} | {meta.description}</title>
+        <title>{meta.title + " | " + meta.description}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
         <meta property="og:url" content={`https://openorganization.org`} />
